b2583875c8f4efbfb3b21ea5059b66eb2a1685d6
Corina Talavera <43788757+Corina-Talavera@users.noreply.github.com>
2018-12-26 13:56:40 -0500

    Create studio_ghibli_characters.rb

---
+++ lib/studio_ghibli_characters.rb
@@ -0,0 +1,11 @@
+module StudioGhibliCharacters
+end
+  
+require 'rest-client'
+require 'json'
+require 'pry'
+
+require_relative "./studio_ghibli_characters/version"
+require_relative "./studio_ghibli_characters/cli"
+require_relative "./studio_ghibli_characters/api"
+require_relative "./studio_ghibli_characters/characters"
